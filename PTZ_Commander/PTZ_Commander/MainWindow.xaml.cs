ce3d7d3de69ffac834666f63326ba24d32544c42
TheEinfachFelix <the.einfach.felix@gmail.com>
2024-01-19 21:09:01 +0100

    added more features

--- PTZ_Commander/PTZ_Commander/MainWindow.xaml.cs
+++ PTZ_Commander/PTZ_Commander/MainWindow.xaml.cs
@@ -5,8 +5,10 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,24 +19,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace PTZ_Comander
+
 {
     
 
     public partial class MainWindow : Window
-    {
+    {        
         private List<TabItem> _tabItems;
         private TabItem _tabAdd;
-        List<Settings> settings;
+        public List<Settings> settings;
         int controllerNR = 0;
         int camNR = 0;
         int controllerINIT = 0;
+
         Vector Cam_Input_Defauld_Offset = new Vector(205,-54);
         Vector m_Cam_Input_Stick_Pos = new Vector();
 
-        DataBinding _Binding = new DataBinding()
+        public DataBinding _Binding = new DataBinding()
         {
             TallyCollor = Brushes.Black,
             ContrName = "not Loaded",
@@ -66,27 +75,30 @@ namespace PTZ_Comander
 
             Cam_Tally_Collor = Brushes.White,
             Cam_Tally_Blink_Collor = Brushes.White
-
         };
+        private MQTT myMQTT = new MQTT();
 
         public MainWindow()
         {
             try
             {
+                
                 DataContext = _Binding;
 
                 InitializeComponent();
-   
                 LoadJson();
-               
+
                 _tabItems = new List<TabItem>(); // initialize tabItem array
 
                 // create add tab
                 _tabAdd = new TabItem();
                 _tabAdd.Header = "+";
                 _tabItems.Add(_tabAdd);
-
+////////////Console.WriteLine(settings[0].ToString());
                 tabDynamic.DataContext = _tabItems;// bind tab control
+                
+                Update();
+
                 tabDynamic.SelectedIndex = 0;
 
                 // load tabs from settings
@@ -94,9 +106,20 @@ namespace PTZ_Comander
                 {
                     this.AddTabItem();
                 }
+
                 // set cam input positions
                 Reset_Stick_Position();
                 Update_Eye_Position();
+
+
+                ///////////////////////////////////////// MQTT Discover
+
+                //my_MQTT_Handler = new MQTT_Handler();
+                //Subscribe("visca/PTZ-01");
+                //Send_msg("visca/ping","");
+                //myMQTT.MQTT_Discover();
+                //Console.WriteLine();
+
             }
             catch (Exception ex)
             {
@@ -110,7 +133,7 @@ namespace PTZ_Comander
             SaveJson();
         }
 
-        private TabItem AddTabItem()
+        public TabItem AddTabItem()
         {
             controllerINIT++;
             TabItem tab = new TabItem();// create new tab item
@@ -243,6 +266,10 @@ namespace PTZ_Comander
             _Binding.ContrName = TheController.Name;
             _Binding.ContrIP = TheController.Ip;
             _Binding.GenA = settings[0].GeneralSettings.a.ToString();
+            _Binding.MQTT_Await_Requ_Duration = settings[0].GeneralSettings.MQTT_Await_Requ_Duration;
+            _Binding.MQTT_Discover_Duration = settings[0].GeneralSettings.MQTT_Discover_Duration;
+
+            
             ////////////////
 
             _Binding.Cam_Name = TheCam.Name;
@@ -254,6 +281,11 @@ namespace PTZ_Comander
 
             _Binding.Cam_Zoom = TheCam.Zoom;
             _Binding.Cam_Fokus = TheCam.Focus;
+
+            ////////////////////
+            _Binding.MQTT_ID = TheController.MQTT_ID;
+            _Binding.MQTT_Input = TheController.MQTT_Input;
+            _Binding.MQTT_Output = TheController.MQTT_Output;
         }
 
         /// Diese Funktion braucht updates vom Json
@@ -270,6 +302,9 @@ namespace PTZ_Comander
             TheController.Name = _Binding.ContrName;
             TheController.Ip = _Binding.ContrIP;
             settings[0].GeneralSettings.a = int.Parse(_Binding.GenA);
+
+             settings[0].GeneralSettings.MQTT_Await_Requ_Duration = _Binding.MQTT_Await_Requ_Duration;
+             settings[0].GeneralSettings.MQTT_Discover_Duration = _Binding.MQTT_Discover_Duration;
             ////////////////
 
             TheCam.Name = _Binding.Cam_Name;
@@ -281,6 +316,11 @@ namespace PTZ_Comander
 
             TheCam.Zoom  = _Binding.Cam_Zoom;
             TheCam.Focus = _Binding.Cam_Fokus;
+
+            ///////////////
+            TheController.MQTT_ID = _Binding.MQTT_ID;
+            TheController.MQTT_Input = _Binding.MQTT_Input;
+            TheController.MQTT_Output = _Binding.MQTT_Output;
         }
 
         //////////////////////////// Cam Input
@@ -289,6 +329,7 @@ namespace PTZ_Comander
         {
             //Console.WriteLine("ComToCam: tally switch");
             settings[0].Controller[controllerNR].Cams[camNR].Tally = !settings[0].Controller[controllerNR].Cams[camNR].Tally;
+            settings[0].Controller[controllerNR].Cams[camNR].Tally_Blink = false;
             Cam_Tally_Update_Collors();
         }
 
@@ -297,6 +338,7 @@ namespace PTZ_Comander
         {
             //Console.WriteLine("ComToCam: tally switch");
             settings[0].Controller[controllerNR].Cams[camNR].Tally_Blink = !settings[0].Controller[controllerNR].Cams[camNR].Tally_Blink;
+            settings[0].Controller[controllerNR].Cams[camNR].Tally = false;
             Cam_Tally_Update_Collors();
         }
 
@@ -312,6 +354,25 @@ namespace PTZ_Comander
             _Binding.Cam_Tally_Blink_Collor = Brushes.White;
             if (TheCam.Tally_Blink)
                 _Binding.Cam_Tally_Blink_Collor = Brushes.PaleVioletRed;
+
+            if (settings[0].Controller[controllerNR].Cams[camNR].Tally)
+            {
+                //_Binding.MQTT_Input
+                //"{\"camera\":{    \"index\":0,    \"port\":0  },  \"command\":\"Call_LED_Off\", \"parameter\":{\"value\":10 }}"
+
+                //myMQTT.MQTT_Push_Msg("esp / PTZ - 01 / push / visca / json","der" );
+            }
+            else if (settings[0].Controller[controllerNR].Cams[camNR].Tally_Blink)
+            {
+                
+            }
+            else
+            {
+
+            }
+
+
+
         }
 
         private void Cam_Input_Joystick_Mouse_Move(object sender, MouseEventArgs e)
@@ -469,11 +530,142 @@ namespace PTZ_Comander
                 settings[0].Controller.Add((Controller)h[0]);
             }
         }
+
+        ////////////////////////// MQTT
+
     }
 
+    public class MQTT:MainWindow
+    {
+        MqttClient client;
+        List<Cam_Ping_Json> MQTT_Discover_Helper = new List<Cam_Ping_Json> { };
+        String MQTT_Recive_Helper;
+        List<string> Subed_Toppics = new List<string>(); // ich Glaube das hat keinen zweck
+
+
+        public MQTT() 
+        {
+
+            client = new MqttClient("192.168.178.116");
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+        }
+
+        ~MQTT()
+        {
+            foreach (var Topic in Subed_Toppics)
+            {
+                client.Unsubscribe(new string[] { Topic });
+            }
+            client.Disconnect();
+        }
+
+        /// <summary>
+        /// Discovers all the cams and Returns a list of all the Returned Objects
+        /// </summary>
+        /// <returns></returns>
+        public void MQTT_Discover()
+        {
+            //////// Send Request
+            string strValue = Convert.ToString("Das ist der Ping von meinem code");
+
+            client.Publish("visca/ping", Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+
+            //////// Sub
+            client.MqttMsgPublishReceived += MQTT_Discover_Received;
+
+            Subed_Toppics.Add("visca/PTZ-01");
+            ////////////////////////////////client.Subscribe(new string[] { "visca/PTZ-01" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+            /*
+            //Thread.Sleep(settings[0].GeneralSettings.MQTT_Discover_Duration);
+
+            //client.Unsubscribe(new string[] { "visca/PTZ-01" });
+            //client.MqttMsgPublishReceived -= MQTT_Discover_Received;
+
+            //if (MQTT_Discover_Helper == new List<Cam_Ping_Json> {})
+            //{
+            //    return new List<Cam_Ping_Json> {};
+            //}
+            //var helper = MQTT_Discover_Helper;
+            //MQTT_Discover_Helper = new List<Cam_Ping_Json> {};
+            //return helper;
+            */
+        }
+        private void MQTT_Discover_Received(object sender, MqttMsgPublishEventArgs e)
+        {
+            /////// Cleaning the input
+            String Json = Encoding.UTF8.GetString(e.Message);
+            Json = "[" + Json + "]";
+            Json = Json.Replace("mqtt-json", "mqttjson");
+
+            /////// Generate Obj and add it to list
+            Cam_Ping_Json helper = JsonConvert.DeserializeObject<List<Cam_Ping_Json>>(Json)[0];
+
+            this.AddTabItem();
+            settings[0].Controller[-1].MQTT_ID = helper.id.ToString();
+            Console.WriteLine(helper.ToString());
+
+
+            MQTT_Discover_Helper.Add(helper);
+        }
+
+        /// <summary>
+        /// Send Payload to Topic but does not await response
+        /// </summary>
+        /// <param name="Topic"></param>
+        /// <param name="Payload"></param>
+        public void MQTT_Push_Msg(string Topic, string Payload)
+        {
+            //client.Publish(Topic, Encoding.UTF8.GetBytes(Convert.ToString(Payload)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            client.Publish("esp/PTZ-01/push/visca/json", Encoding.UTF8.GetBytes("dsafe"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+        }
+
+
+        /// <summary>
+        /// Send Payload to Topic and Return the Response
+        /// </summary>
+        /// <param name="Topic"></param>
+        /// <param name="Payload"></param>
+        /// <returns></returns>
+        public string MQTT_Send_Msg(string inptTopic, string otptTopic, string Payload)
+        {
+            //////// Send Request
+            string strValue = Convert.ToString(Payload);
+
+            client.Publish(inptTopic, Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+
+            //////// Sub
+            client.MqttMsgPublishReceived += MQTT_Response_Received;
+
+            Subed_Toppics.Add(inptTopic);
+            client.Subscribe(new string[] { inptTopic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+            Thread.Sleep(settings[0].GeneralSettings.MQTT_Await_Requ_Duration);
+
+            client.Unsubscribe(new string[] { inptTopic });
+            client.MqttMsgPublishReceived -= MQTT_Response_Received;
+
+            if (MQTT_Recive_Helper == null)
+            {
+                return "";
+            }
+            var helper = MQTT_Recive_Helper;
+            MQTT_Recive_Helper = null;
+            return helper;
+        }
+        private void MQTT_Response_Received(object sender, MqttMsgPublishEventArgs e)
+        {
+            MQTT_Recive_Helper = Encoding.UTF8.GetString(e.Message);
+        }
+    }
+
+    ///////////////////////////////// Json
     public class GeneralSettings
     {
         public int LastViewed { get; set; }
+        public int MQTT_Discover_Duration { get; set; }
+        public int MQTT_Await_Requ_Duration { get; set; }
         public int a { get; set; }
         public int b { get; set; }
     }
@@ -495,6 +687,9 @@ namespace PTZ_Comander
     {
         public string Name { get; set; }
         public string Ip { get; set; }
+        public string MQTT_ID { get; set; }
+        public string MQTT_Input { get; set; }
+        public string MQTT_Output { get; set; }
         public int LastViewed { get; set; }
         public IList<Cam> Cams { get; set; }
     }
@@ -503,9 +698,15 @@ namespace PTZ_Comander
     {
         public GeneralSettings GeneralSettings { get; set; }
         public IList<Controller> Controller { get; set; }
+
+        public override string ToString()
+        {
+            return "\nthe Return of Settings.toString() :->\n" + JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
     }
 
 
+    ///////////////////////////////// Binding
     public class DataBinding : INotifyPropertyChanged
     {
 
@@ -801,6 +1002,62 @@ namespace PTZ_Comander
             }
         }
 
+        ////////////////////////////////////// MQTT //////////////////////////////////////
+        private int _MQTT_Discover_Duration;
+        public int MQTT_Discover_Duration
+        {
+            get { return _MQTT_Discover_Duration; }
+            set
+            {
+                _MQTT_Discover_Duration = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int _MQTT_Await_Requ_Duration;
+        public int MQTT_Await_Requ_Duration
+        {
+            get { return _MQTT_Await_Requ_Duration; }
+            set
+            {
+                _MQTT_Await_Requ_Duration = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        private string _MQTT_ID;
+        public string MQTT_ID
+        {
+            get { return _MQTT_ID; }
+            set
+            {
+                _MQTT_ID = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _MQTT_Input;
+        public string MQTT_Input
+        {
+            get { return _MQTT_Input; }
+            set
+            {
+                _MQTT_Input = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _MQTT_Output;
+        public string MQTT_Output
+        {
+            get { return _MQTT_Output; }
+            set
+            {
+                _MQTT_Output = value;
+                NotifyPropertyChanged();
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -811,7 +1068,48 @@ namespace PTZ_Comander
                 handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
+
+    ///////////////////////////////// MQTT 
+    public class MqttJson
+    {
+        public string input { get; set; }
+        public string output { get; set; }
+    }
+
+    public class Plugins
+    {
+        public MqttJson mqttjson { get; set; }
+    }
+
+    public class Cam_Ping_Json
+    {
+        public long id { get; set; }
+        public string ipv4 { get; set; }
+        public string ipv6g { get; set; }
+        public string ipv6l { get; set; }
+        public Plugins plugins { get; set; }
+        public string visca { get; set; }
+
+        public override string ToString() 
+        { 
+        return "\nthe Return of Cam_Ping_Json.toString() :->\n" + JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+        /*
+         * {"id":13984767495476,
+         * "ipv4":"192.168.178.119",
+         * "ipv6g":"::",
+         * "ipv6l":"::",
+         * "plugins":
+         * { "mqtt-json":
+         * { "input":"esp/PTZ-01/push/visca/json",
+         * "output":"esp/PTZ-01/get/visca/json"} },
+         * "visca":"pong"}
+        */
+    }
+
 }
+
+
 
 /*
  TODO:
@@ -832,7 +1130,8 @@ namespace PTZ_Comander
 -   [ ] Controlls get Status 
 - X [3] Auto Save Cam Settings
 -   [4] Motion EYE Hintergrundbild
--   [3] Add json stuff
+- X [3] Add json stuff
+-   [3] Map Discoverd data to json data
 
 Controlls:
 -   [X] Tally Blink
@@ -850,6 +1149,11 @@ Controlls:
 
 Visca:
 -   [5] add coms mqttnet
--   [2] Discover Cams
+- X [2] Discover Cams
+-   [3] Add Nice Popup for response to discover
+-   [3] Write Discovered data into json
+-   [2] error for no resopnse
+-   [2] beim erwarten von requests auch vorzeitig abbrechen können
+-   [3] unlimited discover indem das ganze parsing in MQTT_Discover_Received passiert
 
  */
